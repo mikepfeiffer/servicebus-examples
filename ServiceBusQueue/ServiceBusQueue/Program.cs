7bc4d367a4eef011bb277b4f9828b7f21a5deba9
Mike Pfeiffer <mike.pfeiffer@gmail.com>
2017-12-05 12:34:25 -0700

    added queue project

---
+++ ServiceBusQueue/ServiceBusQueue/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ServiceBusQueue
+{
+    class Program
+    {
+        private static string _conn = "Endpoint=sb://<namespace>.servicebus.windows.net/;SharedAccessKeyName=<Key Name>;SharedAccessKey=<Key>;EntityPath=<Queue Name>";
+        private static QueueClient _client = QueueClient.CreateFromConnectionString(_conn);
+
+        static void Main(string[] args)
+        {
+        }
+
+        static void SendMessage(string msg)
+        {
+            var message = new BrokeredMessage(msg);
+            _client.Send(message);
+        }
+
+        static void ReadMessage()
+        {
+            var options = new OnMessageOptions
+            {
+                AutoComplete = false
+            };
+
+            _client.OnMessage(m =>
+            {
+                var msg = m.GetBody<string>();
+
+                if (msg == "Hello World!")
+                {
+                    Console.WriteLine(msg);
+                    m.Complete();
+                }
+
+            }, options);
+        }
+    }
+}
