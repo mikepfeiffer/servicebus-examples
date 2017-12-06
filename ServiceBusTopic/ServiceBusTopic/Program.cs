c35613e234515ec86894baead5d84af296dccb82
Mike Pfeiffer <mike.pfeiffer@gmail.com>
2017-12-06 12:13:44 -0700

    added service bus topic project

---
+++ ServiceBusTopic/ServiceBusTopic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ServiceBusTopic
+{
+    class Program
+    {
+        private static string _conn = "Endpoint=sb://<namespace>.servicebus.windows.net/;SharedAccessKeyName=<policy name>;SharedAccessKey=<key>";
+        private static string _topic = "<topic name>";
+        
+        static void Main(string[] args)
+        {
+        }
+
+        static void SendMessage(string message)
+        {
+            var topicClient = TopicClient.CreateFromConnectionString(_conn, _topic);
+            var msg = new BrokeredMessage(message);
+            topicClient.Send(msg);
+        }
+
+        static void ReadMessage()
+        {
+            var subClient = SubscriptionClient.CreateFromConnectionString(_conn, _topic, "<subscription name>");
+            subClient.OnMessage(m =>
+            {
+                Console.WriteLine(m.GetBody<string>());
+            });
+        }
+    }
+}
---
+++ ServiceBusTopic/ServiceBusTopic/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ServiceBusTopic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ServiceBusTopic")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fad6fc28-710d-4779-8b6a-f8fa77c17eb6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
